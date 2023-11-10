c92b3e8bd77e7fc189c1613fffca30160740336e
Sayed Rafiul Islam <sayed80@student.sust.edu>
2023-11-10 20:56:56 +0600

    cam shop1

--- app/camShop/page.js
+++ app/camShop/page.js
@@ -22,10 +22,10 @@ const CamShop = async () => {
         <div className='w-screen animate__animated animate__fadeInLeft'>
             <h1 className='text-5xl pt-20 text-center font-bold text-white'>CAM SHOP</h1>
           
-                <div className='grid lg:grid-cols-3 grid-cols-1 gap-y-10 mx-auto w-5/6 py-10'>
+                <div className='grid lg:grid-cols-3 grid-cols-1 gap-y-10 mx-auto w-3/4 lg:pl-20 pl-0 py-10'>
                 {
                     products.map(({_id,name,slug,images,currency,price}) => (   
-                            <div className='product-card lg:w-3/4 full lg:mx-0 mx-auto text-center text-white rounded-lg' key={_id}>
+                            <div className='product-card lg:w-3/4 w-full lg:mx-0 mx-auto text-center text-white rounded-lg' key={_id}>
                                 <Link  href={`/camShop/${slug}`}><div className='overflow-hidden mt-5'><img  className='mx-auto rounded-b-xl' width={300} src={urlFor(images).url()}/></div></Link>
                                 <Link  href={`/camShop/${slug}`}><h3 className='font-bold mt-3'>{name}</h3>
                                 <p className='text-orange-400'>{currency} {price}</p>
