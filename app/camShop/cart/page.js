b556d78e72e657495262f38602aa83b6e0fa2ca1
Sayed Rafiul Islam <sayed80@student.sust.edu>
2023-11-12 00:36:50 +0600

    cart remove logic

--- app/camShop/cart/page.js
+++ app/camShop/cart/page.js
@@ -19,7 +19,27 @@ function urlFor(source) {
 export default function Cart() {
 
   const [cartItems,setCartItems] = useState([])
-  console.log(cartItems)
+  const [tempCart,setTempCart] = useState([])
+  const [modal,setModal] = useState(true)
+
+
+  const handleCartItem = (_id,size) => {
+        const newCart = cartItems.filter(cartItem => cartItem._id !== _id || cartItem.size !==size)
+        setTempCart(newCart)
+        setModal(false);
+  }
+
+  const confirmCart = (confirm) => {
+    if(confirm) {
+      setCartItems(tempCart)
+      localStorage.setItem("cartItems", JSON.stringify(tempCart))
+      setModal(true)
+    }
+    else {
+      setModal(true)
+    }
+
+  }
   
 
   
@@ -34,60 +54,68 @@ export default function Cart() {
           setCartItems(data)
       }
     }
-   },[])
+   },[modal])
  
 
   return (
-    <div className='cart h-screen w-screen'>
+    <div className=' h-screen w-screen'>
+      <div className={modal ? 'modal-off' : 'modal-on'}>
+              <h3>Are you sure?</h3>
+              <div className='text-white'>
+                <button className='mx-2' onClick={()=>confirmCart(true)}>Yes</button>
+                <button className='mx-2' onClick={()=>confirmCart(false)}>No</button>
+              </div>
+        </div>
       <div className='cart-bg w-screen h-full'>
-       
+           
       </div>
       <h1 className='text-2xl text-white'></h1>
-      <div class="mt-24 mx-auto overflow-x-auto w-5/6">
-      <table class="table text-center">
-        <thead>
-          <tr>
-            <th>Product No</th>
-            <th>Thumbnail</th>
-            <th>Product Name</th>
-            <th>Quantity</th>
-            <th>Size</th>
-            <th>Price</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-       {
-        cartItems ? 
-        cartItems.map(({name,image,price,quantity,size,_id},index)=>
-        
-          <tr key={_id}>
-          <td>{index + 1}</td>
-            <td>
-              <div class="flex items-center justify-center space-x-3">
-                <div class="avatar">
-                  <div class="mask mask-squircle w-12 h-12">
-                    <img src={urlFor(image).url()} alt="Avatar Tailwind CSS Component" />
-                  </div>
+      <div class="mt-24 cart mx-auto flex">
+        <table class="table text-center text-white">
+          <thead>
+            <tr>
+              <th>Product No</th>
+              <th>Thumbnail</th>
+              <th>Product Name</th>
+              <th>Quantity</th>
+              <th>Size</th>
+              <th>Price</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+        {
+          cartItems ? 
+          cartItems.map(({name,image,price,quantity,size,_id},index)=>
+          
+            <tr key={_id} className='row'>
+            <td>{index + 1}</td>
+              <td>
+                <div class="flex justify-center">
+                    <div class="w-12 h-12 avatar rounded-full">
+                      <img className='rounded-full' src={urlFor(image).url()} alt="Avatar Tailwind CSS Component" />
+                    </div>
                 </div>
-              </div>
-            </td>
-            <td>{name}</td>
-            <td>{quantity}</td>
-            <td>{size}</td>
-            <td>{quantity*price}</td>
-            <th>
-              <button class="btn btn-ghost btn-xs">Remove</button>
-            </th>
-          </tr>
+              </td>
+              <td>{name}</td>
+              <td>{quantity}</td>
+              <td>{size}</td>
+              <td className='py-6'>{quantity*price}</td>
+              <td>
+                <button onClick={()=>handleCartItem(_id,size)} className='py-1 px-4 cart-btn'>Remove</button>
+              </td>
+            </tr>
+          
+          )
         
-        )
-       
-      :
-      <h1 className='text-3xl text-red-300'>no data</h1>
-       }
-       </tbody>
-      </table>
+        :
+        <h1 className='text-3xl text-red-300'>no data</h1>
+        }
+        </tbody>
+        </table>
+        <div className='checkout-box'>
+
+        </div>
       </div>
 
     </div>
