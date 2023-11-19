82f84d8e3d5c2d43993d95570e19e313ecd3cf8e
Sayed Rafiul Islam <sayed80@student.sust.edu>
2023-11-20 00:51:41 +0600

    no cache removed

--- utils/getComments.js
+++ utils/getComments.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default async function getComments() {
     try {
-        const res = await fetch(`http://localhost:3000/api/comments`,{cache : "no-store"})
+        const res = await fetch(`http://localhost:3000/api/comments`)
         if (!res.ok) {
           throw new Error("faild to fetch comments")
         }
