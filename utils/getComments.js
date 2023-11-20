4ec7fa9bcb8a7a4354151d101d1059cda172080f
Sayed Rafiul Islam <sayed80@student.sust.edu>
2023-11-20 21:59:02 +0600

    dlt btn

--- utils/getComments.js
+++ utils/getComments.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default async function getComments(slug) {
     try {
-        const res = await fetch(`http://localhost:3000/api/comments?slug=${slug}`,{cache : "no-store"})
+        const res = await fetch(`https://cam-sust.vercel.app/api/comments?slug=${slug}`,{cache : "no-store"})
         if (!res.ok) {
           throw new Error("faild to fetch comments")
         }
