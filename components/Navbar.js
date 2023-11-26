a0285929b2cbd3d0edad876b4ee0e27f0ac60152
Sayed Rafiul Islam <sayed80@student.sust.edu>
2023-11-26 16:06:42 +0600

    img opt

--- components/Navbar.js
+++ components/Navbar.js
@@ -87,7 +87,7 @@ useEffect(() => window.addEventListener('scroll', changeBackground), []);
                     {
                         user ?
                         <button onClick={logOut} className="lg:border border-red-600 text-red-600 px-4 my-2 lg:mr-5 ml-36 rounded-sm hover:bg-red-600 hover:text-white transition">Sign Out</button> :
-                    <button onClick={googleSignIn} className="navItem mr-5 lg:ml-36">Sign In</button> 
+                    <button onClick={googleSignIn} className="navItem mr-5 ml-36">Sign In</button> 
                     }
                     <Link prefetch as='/camShop/cart' onClick={()=>setVisible(true)} className={visible ? 'cart-ico-1 my-auto cart-ico' : 'cart-ico-2  my-auto cart-ico' } href='/camShop/cart' ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
